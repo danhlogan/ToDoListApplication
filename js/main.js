5450f22f3b56a312276dd812be82cc17c94869bb
danhlogan <logan0839@student.cptc.edu>
2020-06-08 21:40:08 -0700

    Added To Do List and Completed To Do sections

--- js/main.js
+++ js/main.js
@@ -8,16 +8,41 @@ window.onload = function () {
     addNewTask.onclick = main;
 };
 function main() {
-    createTask();
+    if (isValid()) {
+        createTask();
+    }
+    var isComplete = document.querySelectorAll("input[type='checkbox']");
+    for (var i = 0; i < isComplete.length; i++) {
+        isComplete[i].onclick = completedTask;
+    }
+}
+function isValid() {
+    if (document.getElementById("newTask").value == "") {
+        document.getElementById("newTaskSpan").innerText = "Please add a new task";
+        return false;
+    }
+    return true;
 }
 function createTask() {
     var item = new ToDoItem();
     item.newTask = document.getElementById("newTask").value;
+    item.dueDate = document.getElementById("dueDate").value;
     var newListItem = document.createElement("LI");
-    var innerText = document.createTextNode(item.newTask);
+    if (item.dueDate == "") {
+        var innerText = document.createTextNode(item.newTask + " No due date");
+    }
+    else {
+        var innerText = document.createTextNode(item.newTask + " Complete by " + item.dueDate);
+    }
     var isDone = document.createElement("INPUT");
     isDone.setAttribute("type", "checkbox");
     newListItem.appendChild(isDone);
     newListItem.appendChild(innerText);
     document.getElementById("taskList").appendChild(newListItem);
+    document.getElementById("newTaskSpan").innerText = "";
+    document.getElementById("newTask").value = "";
+    document.getElementById("dueDate").value = "";
+}
+function completedTask() {
+    alert("IT WORKED ");
 }
--- ts/main.ts
+++ ts/main.ts
@@ -1,10 +1,8 @@
 class ToDoItem
 {
     newTask: string;
-    dueDate: Date;
+    dueDate: string;
 }
-
-
 
 window.onload = function()
 {
@@ -15,21 +13,61 @@ window.onload = function()
 
 function main(): void
 {
-    createTask()
+    if(isValid())
+    {
+        createTask()
+    }
+
+    // Moves from To Do to Done 
+    let isComplete = document.querySelectorAll("input[type='checkbox']")
+    for(let i = 0; i < isComplete.length; i++)
+        {
+            isComplete[i].onclick = completedTask
+        }
+}
+
+function isValid(): boolean
+{
+    if(document.getElementById("newTask").value == "")
+    { 
+        document.getElementById("newTaskSpan").innerText = "Please add a new task"
+        return false
+    }
+    return true
 }
 
 function createTask():void
 {
-    let item = new ToDoItem()
-    item.newTask = document.getElementById("newTask").value
-    //item.dueDate = document.getElementById("dueDate").innerText
-    let newListItem = document.createElement("LI")
-    let innerText = document.createTextNode(item.newTask);
-    let isDone = document.createElement("INPUT")
-    isDone.setAttribute("type","checkbox")
-    newListItem.appendChild(isDone)
+    let item = new ToDoItem();
+    item.newTask = document.getElementById("newTask").value;
+    item.dueDate = document.getElementById("dueDate").value;
+    let newListItem = document.createElement("LI");
+
+    if(item.dueDate == "")
+    { 
+        let innerText = document.createTextNode(item.newTask + " No due date")
+    }
+
+    else
+    {
+        let innerText = document.createTextNode(item.newTask + " Complete by " + item.dueDate)
+    }
+    
+    let isDone = document.createElement("INPUT");
+    isDone.setAttribute("type", "checkbox");
+    newListItem.appendChild(isDone);
     newListItem.appendChild(innerText);
     document.getElementById("taskList").appendChild(newListItem);
+    
+    // Resets text boxes to blank
+    document.getElementById("newTaskSpan").innerText = ""
+    document.getElementById("newTask").value = ""
+    document.getElementById("dueDate").value = ""
+}
+
+function completedTask()
+{
+    alert("IT WORKED ")
 }
 
 
